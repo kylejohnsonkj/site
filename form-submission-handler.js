863bcc683c7a90233d45bbf9acf5d4f844f095d4
Kyle Johnson <kyjohnson09@gmail.com>
2021-07-31 20:44:15 -0500

    Enforce formatting

--- form-submission-handler.js
+++ form-submission-handler.js
@@ -1,104 +1,104 @@
 (function() {
-  function isRobot() {
-    var honeypot = document.getElementById("honeypot").value;
-    if (honeypot == "human") {
-      console.log("Welcome Human!");
-    } else {
-      console.log("Robot Detected!");
-      return true;
-    }
-  }
-
-  // get all data in form and return object
-  function getFormData() {
-    var form = document.getElementById("gform");
-    var elements = form.elements;
-
-    var fields = Object.keys(elements).filter(function(k) {
-          return (elements[k].name !== "honeypot");
-    }).map(function(k) {
-      if(elements[k].name !== undefined) {
-        return elements[k].name;
-      // special case for Edge's html collection
-      }else if(elements[k].length > 0){
-        return elements[k].item(0).name;
-      }
-    }).filter(function(item, pos, self) {
-      return self.indexOf(item) == pos && item;
-    });
-
-    var formData = {};
-    fields.forEach(function(name){
-      var element = elements[name];
-
-      // singular form elements just have one value
-      formData[name] = element.value;
-
-      // when our element has multiple items, get their values
-      if (element.length) {
-        var data = [];
-        for (var i = 0; i < element.length; i++) {
-          var item = element.item(i);
-          if (item.checked || item.selected) {
-            data.push(item.value);
-          }
+    function isRobot() {
+        var honeypot = document.getElementById("honeypot").value;
+        if (honeypot == "human") {
+            console.log("Welcome Human!");
+        } else {
+            console.log("Robot Detected!");
+            return true;
         }
-        formData[name] = data.join(', ');
-      }
-    });
-
-    console.log(formData);
-    return formData;
-  }
-
-  function handleFormSubmit(event) {  // handles form submit without any jquery
-    event.preventDefault();           // we are submitting via xhr below
-    var data = getFormData();         // get the values submitted in the form
-
-    if (isRobot()) {  // if form is filled, form will not be submitted
-        document.getElementById("gform").style.display = "none"; // hide form
-        var honeypotMessage = document.getElementById("honeypot_message");
-        if (honeypotMessage) {
-          honeypotMessage.style.display = "block";
-        }
-        return false;
     }
 
-    disableAllButtons(event.target);
-    var url = "https://script.google.com/macros/s/AKfycbyrcqSm5KuUpjdGdLG7AqDB333ZlX5eqqPGiL3pP32LL5IEzyK1/exec"
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', url);
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xhr.onreadystatechange = function() {
-      console.log( xhr.status, xhr.statusText )
-      console.log(xhr.responseText);
-      document.getElementById("gform").style.display = "none"; // hide form
-      var thankYouMessage = document.getElementById("thankyou_message");
-      if (thankYouMessage) {
-        thankYouMessage.style.display = "block";
-      }
-      return;
+    // get all data in form and return object
+    function getFormData() {
+        var form = document.getElementById("gform");
+        var elements = form.elements;
+
+        var fields = Object.keys(elements).filter(function(k) {
+            return (elements[k].name !== "honeypot");
+        }).map(function(k) {
+            if (elements[k].name !== undefined) {
+                return elements[k].name;
+                // special case for Edge's html collection
+            } else if (elements[k].length > 0) {
+                return elements[k].item(0).name;
+            }
+        }).filter(function(item, pos, self) {
+            return self.indexOf(item) == pos && item;
+        });
+
+        var formData = {};
+        fields.forEach(function(name) {
+            var element = elements[name];
+
+            // singular form elements just have one value
+            formData[name] = element.value;
+
+            // when our element has multiple items, get their values
+            if (element.length) {
+                var data = [];
+                for (var i = 0; i < element.length; i++) {
+                    var item = element.item(i);
+                    if (item.checked || item.selected) {
+                        data.push(item.value);
+                    }
+                }
+                formData[name] = data.join(', ');
+            }
+        });
+
+        console.log(formData);
+        return formData;
+    }
+
+    function handleFormSubmit(event) { // handles form submit without any jquery
+        event.preventDefault(); // we are submitting via xhr below
+        var data = getFormData(); // get the values submitted in the form
+
+        if (isRobot()) { // if form is filled, form will not be submitted
+            document.getElementById("gform").style.display = "none"; // hide form
+            var honeypotMessage = document.getElementById("honeypot_message");
+            if (honeypotMessage) {
+                honeypotMessage.style.display = "block";
+            }
+            return false;
+        }
+
+        disableAllButtons(event.target);
+        var url = "https://script.google.com/macros/s/AKfycbyrcqSm5KuUpjdGdLG7AqDB333ZlX5eqqPGiL3pP32LL5IEzyK1/exec"
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', url);
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        xhr.onreadystatechange = function() {
+            console.log(xhr.status, xhr.statusText)
+            console.log(xhr.responseText);
+            document.getElementById("gform").style.display = "none"; // hide form
+            var thankYouMessage = document.getElementById("thankyou_message");
+            if (thankYouMessage) {
+                thankYouMessage.style.display = "block";
+            }
+            return;
+        };
+
+        // url encode form data for sending as post data
+        var encoded = Object.keys(data).map(function(k) {
+            return encodeURIComponent(k) + "=" + encodeURIComponent(data[k])
+        }).join('&')
+        xhr.send(encoded);
+    }
+
+    function loaded() {
+        console.log("Contact form submission handler loaded successfully.");
+        // bind to the submit event of our form
+        var form = document.getElementById("gform");
+        form.addEventListener("submit", handleFormSubmit, false);
     };
+    document.addEventListener("DOMContentLoaded", loaded, false);
 
-    // url encode form data for sending as post data
-    var encoded = Object.keys(data).map(function(k) {
-      return encodeURIComponent(k) + "=" + encodeURIComponent(data[k])
-    }).join('&')
-    xhr.send(encoded);
-  }
-
-  function loaded() {
-    console.log("Contact form submission handler loaded successfully.");
-    // bind to the submit event of our form
-    var form = document.getElementById("gform");
-    form.addEventListener("submit", handleFormSubmit, false);
-  };
-  document.addEventListener("DOMContentLoaded", loaded, false);
-
-  function disableAllButtons(form) {
-    var buttons = form.querySelectorAll("button");
-    for (var i = 0; i < buttons.length; i++) {
-      buttons[i].disabled = true;
+    function disableAllButtons(form) {
+        var buttons = form.querySelectorAll("button");
+        for (var i = 0; i < buttons.length; i++) {
+            buttons[i].disabled = true;
+        }
     }
-  }
 })();
--- kjapps.js
+++ kjapps.js
@@ -22,26 +22,26 @@ $(document).ready(function() {
     /* Play Embedded YouTube Video */
     $("#video").click(function(event) {
         event.preventDefault();
-        
+
         var link = this.href.substring(this.href);
         var embeddedLink = link.replace("watch?v=", "embed/");
         var flags = "?autoplay=1&iv_load_policy=3";
         var newLink = embeddedLink.concat(flags);
-        
-	   if ($("#fullContainer").css('opacity') == 1) {
-		  $("#fullContainer").animate({opacity:0.3}, 1000);
-          $("#videoContainer").html("<iframe allowfullscreen=\"allowfullscreen\" style=\"border: 3px solid #EEE;\" src=\"" + newLink + "\"></iframe>");
-          $("#videoContainer").css("display", "inline");
-	   }
+
+        if ($("#fullContainer").css('opacity') == 1) {
+            $("#fullContainer").animate({ opacity: 0.3 }, 1000);
+            $("#videoContainer").html("<iframe allowfullscreen=\"allowfullscreen\" style=\"border: 3px solid #EEE;\" src=\"" + newLink + "\"></iframe>");
+            $("#videoContainer").css("display", "inline");
+        }
     });
 
     /* Hide Embedded YouTube Video */
     $("#fullContainer").click(function() {
-	   if ($("#fullContainer").css('opacity') != 1) {
-		  $("#fullContainer").animate({opacity:1}, 1000);
-           $("#videoContainer").html("");
-          $("#videoContainer").css("display", "none"); 
-	   }
+        if ($("#fullContainer").css('opacity') != 1) {
+            $("#fullContainer").animate({ opacity: 1 }, 1000);
+            $("#videoContainer").html("");
+            $("#videoContainer").css("display", "none");
+        }
     });
 
     ajaxRedirect();
@@ -73,25 +73,25 @@ function ajaxRedirect(event) {
     /* Page Transitions (continued) */
     $(".topnav a").click(function(event) {
         event.preventDefault();
-        
+
         var from = getCurrentFileName();
         var to = this.href.substring(this.href.lastIndexOf("/") + 1);
         if (to === "") {
             to = "/";
         }
-        
+
         $("#header").hide(1000);
         $(".content").hide(1000, function() {
             $.ajax({
-               url: to,
-               type: 'GET',
-               success: function(data) {
-                   $('.content').html($(data).find('.content').html());
-                   window.location.replace(to);
-               }
-            }); 
+                url: to,
+                type: 'GET',
+                success: function(data) {
+                    $('.content').html($(data).find('.content').html());
+                    window.location.replace(to);
+                }
+            });
         });
-        
+
         if ($(window).width() <= 820 && event.target.id != "logo") {
             responsiveNavBar();
         }
@@ -100,12 +100,16 @@ function ajaxRedirect(event) {
 
 /* Responsive Navigation Bar */
 function responsiveNavBar() {
-    
+
     // dirty but it works
     var x = document.getElementById("navbar");
     var current = getCurrentFileName();
-    var a = "", b = "", c = "", d = "", e = "";
-    
+    var a = "",
+        b = "",
+        c = "",
+        d = "",
+        e = "";
+
     if (current === "index.html" || current === "index" || current === "/") {
         a = "current";
     } else if (current === "apps.html" || current === "apps") {
@@ -117,12 +121,12 @@ function responsiveNavBar() {
     } else if (current === "contact.html" || current === "contact") {
         e = "current";
     } else {
-	   a = "current";
+        a = "current";
     }
-    
+
     if (x.className === "topnav") {
         x.className += " responsive";
-        
+
         $("#container").css("-webkit-transform", "translateZ(0px)");
         $("#container").css("-webkit-transform", "translate3d(0,0,0)");
         $("#container").css("-webkit-perspective", "1000");
@@ -130,27 +134,27 @@ function responsiveNavBar() {
         $(".topnav").css("border-bottom-style", "solid");
         $(".topnav").css("border-bottom-width", "2px");
         $(window).scrollTop(0);
-        
+
         $(".topnav").html("<a href=\"/\" id=\"logo\">kylejohnson<\/a><a href=\"\" id=\"blank\"><\/a><a href=\"/\" id=\"" + a + "\">about me<\/a><a href=\"apps\" id=\"" + b + "\">my apps<\/a><a href=\"resume\" id=\"" + c + "\">resume<\/a><a href=\"interests\" id=\"" + d + "\">interests<\/a><a href=\"contact\" id=\"" + e + "\">contact<\/a><a href=\"javascript:void(0);\" id=\"icon\" onclick=\"responsiveNavBar()\">&#9776;<\/a>");
-        
+
     } else {
         x.className = "topnav";
-        
+
         $("#container").css("-webkit-transform", "");
         $("#container").css("-webkit-transform", "");
         $("#container").css("-webkit-perspective", "");
         $(".topnav").css("border-bottom-color", "#fff");
         $(".topnav").css("border-bottom-style", "none");
         $(".topnav").css("border-bottom-width", "0px");
-        
+
         $(".topnav").html("<a href=\"/\" id=\"logo\">kylejohnson<\/a><a href=\"javascript:void(0);\" id=\"icon\" onclick=\"responsiveNavBar()\">&#9776;<\/a><a href=\"contact\" id=\"" + e + "\">contact<\/a><a href=\"interests\" id=\"" + d + "\">interests<\/a><a href=\"resume\" id=\"" + c + "\">resume<\/a><a href=\"apps\" id=\"" + b + "\">my apps<\/a><a href=\"/\" id=\"" + a + "\">about me<\/a>");
     }
-    
+
     ajaxRedirect();
 }
 
 function submitForm() {
-    document.getElementById("honeypot").value = "human";    // spam protection
+    document.getElementById("honeypot").value = "human"; // spam protection
     $("input[type=submit]").hide();
     return true;
 }
